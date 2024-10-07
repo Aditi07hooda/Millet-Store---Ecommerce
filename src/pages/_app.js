6d3d1aec49f476b98d0d694b9957835222c3dd5a
gowthaman <gowthaman.b@m2pfintech.com>
2024-10-07 15:12:19 +0530

    load from localStorage the session

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,59 +1,56 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import store from "../store/store";
-import { Provider } from 'react-redux';
-import { useEffect, useState } from "react";
+import {Provider} from 'react-redux';
+import {useEffect, useState} from "react";
 import Loader from "@/components/UI/Loader";
 import MobileBottomNavbar from "@/components/UI/MobileBottomNavbar";
 import Footer from "@/components/Footer";
 
-export default function App({ Component, pageProps }) {
-  const base_url = process.env.NEXT_PUBLIC_BASE_URL;
-  const brand_id = process.env.NEXT_PUBLIC_BRAND_ID;
+export default function App({Component, pageProps}) {
+    const base_url = process.env.NEXT_PUBLIC_BASE_URL;
+    const brand_id = process.env.NEXT_PUBLIC_BRAND_ID;
 
-  const [sessionId, setSessionId] = "";
-  const [loading, setLoading] = useState(true);
+    const [sessionId, setSessionId] = useState(localStorage.getItem("sessionId") || "");
+    const [loading, setLoading] = useState(true);
 
-  const fetchSession = async () => {
-    try {
-      const response = await fetch(`${base_url}/store/${brand_id}/init`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    const fetchSession = async () => {
+        setLoading(true);
 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch session: ${response.status} ${response.statusText}`);
-      }
+        try {
+            if (!sessionId) {
+                const response = await fetch(`${base_url}/store/${brand_id}/init`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
 
-      const data = await response.json();
-      setSessionId(data.session);
-      localStorage.setItem("sessionId", data.session);
-    } catch (error) {
-      console.error("Error fetching session:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+                if (response.ok) {
+                    const data = await response.json();
+                    setSessionId(data.session);
+                    localStorage.setItem("sessionId", data.session);
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching session:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    const userData = localStorage.getItem('userData');
-    if (!userData) {
-      fetchSession();
-    } else {
-      setLoading(false); 
-    }
-  }, []);
+    useEffect(async () => {
+        await fetchSession();
+    }, []);
 
-  if (loading) return <Loader />;
+    if (loading) return <Loader/>;
 
-  return (
-    <Provider store={store}>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-      <MobileBottomNavbar />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Navbar/>
+            <Component {...pageProps} />
+            <Footer/>
+            <MobileBottomNavbar/>
+        </Provider>
+    );
 }
